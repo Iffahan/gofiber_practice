50224642dbd9b9eaeaca527bd0e3091de6f019cb
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-09-08 20:51:50 +0700

    json respone

--- main.go
+++ main.go
@@ -11,11 +11,51 @@ func main() {
  // fiber instance
  app := fiber.New()
 
- // routes 
- app.Get("/", func(c *fiber.Ctx) error {
+ // routes
+
+ app.Get("/", func(c *fiber.Ctx) error { // STRING
   return c.SendString("hello world 🌈")
  })
- 
-// app listening at PORT: 3000
+
+ app.Get("/info", func(c *fiber.Ctx) error { // JSON
+  return c.JSON(fiber.Map{
+   "msg":     "hello world 🚀",
+   "go":      "fiber 🥦",
+   "boolean": true,
+   "number":  1234,
+  })
+ })
+
+ // app listening at PORT: 3000
+ app.Listen(":3000")
+}package main
+
+import (
+ "fmt"
+ "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+ fmt.Println("hello world")
+
+ // fiber instance
+ app := fiber.New()
+
+ // routes
+
+ app.Get("/", func(c *fiber.Ctx) error { // STRING
+  return c.SendString("hello world")
+ })
+
+ app.Get("/info", func(c *fiber.Ctx) error { // JSON
+  return c.JSON(fiber.Map{
+   "msg":     "hello world 🚀",
+   "go":      "fiber 🥦",
+   "boolean": true,
+   "number":  1234,
+  })
+ })
+
+ // app listening at PORT: 3000
  app.Listen(":3000")
 }
