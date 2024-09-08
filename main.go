17317e305b5348518cb0a78d8efcb603937d8562
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-09-08 20:28:46 +0700

    first api

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-  "fmt"
+ "fmt"
+ "github.com/gofiber/fiber/v2"
 )
 
 func main() {
-  fmt.Printf("Hello world");
+ fmt.Println("hello world")
+
+ // fiber instance
+ app := fiber.New()
+
+ // routes 
+ app.Get("/", func(c *fiber.Ctx) error {
+  return c.SendString("hello world 🌈")
+ })
+ 
+// app listening at PORT: 3000
+ app.Listen(":3000")
 }
